0053c5eef50c0717c4f9cb524aafabe2b3c8b46d
rtoledo_dev <rodrigo@rtoledo.inf.br>
2024-02-27 18:26:12 -0300

    Add feature image display in Projects and Tasks components

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,8 @@
 // src/components/Projects.js
 import React from 'react';
 import { useProjectContext } from '../context/ProjectContext';
+import axios from 'axios';
+const baseURL = axios.defaults.baseURL;
 
 const Projects = () => {
   const { projects, completeProject, isLoadingProjects } = useProjectContext();
@@ -19,7 +21,14 @@ const Projects = () => {
         <tbody>
           {!isLoadingProjects && projects.map((project) => (
             <tr key={project.id}>
-              <td className="border border-gray-200 px-4 py-2">{project.title}</td>
+              <td className="border border-gray-200 px-4 py-2">
+                {project.title}
+                <>
+                  {project.feature_image_url && (
+                    <img src={baseURL + project.feature_image_url} alt="Feature Image" />
+                  )}
+                </>
+              </td>
               <td className="border border-gray-200 px-4 py-2">
                 {project.completed_at ? (
                   <span className="text-green-500">Completed</span>
