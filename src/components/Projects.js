bfd92b847771ba573f47761845da031167474347
Rodrigo Toledo <rodrigotoledo@Rodrigos-MacBook-Pro.local>
2024-10-01 11:35:09 -0300

    Update React Query version and improve UI layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -25,65 +25,48 @@ const Projects = () => {
           New project
         </Link>
       </div>
-      <table className="min-w-full border border-gray-200 mt-4">
-        <thead>
-          <tr>
-            <th className="border border-gray-200 px-4 py-2 text-left">Project</th>
-            <th className="border border-gray-200 px-4 py-2 text-left">Status</th>
-            <th className="border border-gray-200 px-4 py-2 text-left">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {!isLoadingProjects && projects?.map((project) => (
-            <tr key={project.id}>
-              <td className="border border-gray-200 px-4 py-2">
-                <div className='items-center space-x-2 flex-row flex'>
-                  <div>
-                    {project.feature_image_url && (
-                      <img src={baseURL + project.feature_image_url} alt="Feature Image" className='w-20' />
-                    )}
-                  </div>
-                  <div>
-                    {project.title}
-                  </div>
-                </div>
-              </td>
-              <td className="border border-gray-200 px-4 py-2">
+      <div className='grid grid-cols-2 md:grid-cols-2 gap-4 my-10'>
+        {!isLoadingProjects && projects?.map((project) => (
+          <div key={project.id} className='p-4 rounded-md border-1 border space-y-4 shadow-lg'>
+            <div className='items-center space-x-2 flex-row flex'>
+              {project.feature_image_url && (
+                <img src={baseURL + project.feature_image_url} alt="Feature Image" className='w-20' />
+              )}
+              <div>
+                <h1 className='text-xl mb-2'>{project.title}</h1>
                 {project.completed_at ? (
-                  <span className="text-green-500">Completed</span>
+                  <span className="bg-green-500 p-1 text-white rounded-lg text-sm">Completed</span>
                 ) : (
-                  <span className="text-yellow-500">Pending</span>
+                  <span className="bg-yellow-500 p-1 text-white rounded-lg text-sm">Pending</span>
                 )}
-              </td>
-              <td className="border border-gray-200 px-4 py-2">
-                <div className="flex space-x-2">
-
-                  {!project.completed_at && (
-                    <Link
-                    className="bg-blue-500 text-white p-2 rounded text-nowrap"
-                    onClick={() => completeProject(project)}
-                    >
-                      Mark as Completed
-                    </Link>
-                  )}
-                  <Link
-                    to={`/projects/${project.id}/edit`}
-                    className="bg-blue-500 text-white p-2 rounded"
-                  >
-                    Edit
-                  </Link>
-                  <Link
-                    className="bg-red-500 text-white p-2 rounded"
-                    onClick={() => destroyProject(project)}
-                  >
-                    Destroy
-                  </Link>
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+              </div>
+            </div>
+            
+            <div className="flex space-x-2">
+              {!project.completed_at && (
+                <Link
+                className="bg-blue-500 text-white p-1 rounded text-nowrap"
+                onClick={() => completeProject(project)}
+                >
+                  Mark as Completed
+                </Link>
+              )}
+              <Link
+                to={`/projects/${project.id}/edit`}
+                className="bg-blue-500 text-white p-1 rounded"
+              >
+                Edit
+              </Link>
+              <Link
+                className="bg-red-500 text-white p-1 rounded"
+                onClick={() => destroyProject(project)}
+              >
+                Destroy
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -25,64 +25,48 @@ const Tasks = () => {
           New task
         </Link>
       </div>
-      <table className="min-w-full border border-gray-200 mt-4">
-        <thead>
-          <tr>
-            <th className="border border-gray-200 px-4 py-2 text-left">Task</th>
-            <th className="border border-gray-200 px-4 py-2 text-left">Status</th>
-            <th className="border border-gray-200 px-4 py-2 text-left">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {!isLoadingTasks && tasks?.map((task) => (
-            <tr key={task.id}>
-              <td className="border border-gray-200 px-4 py-2">
-                <div className='items-center space-x-2 flex-row flex'>
-                  <div>
-                    {task.feature_image_url && (
-                      <img src={baseURL + task.feature_image_url} alt="Feature Image" className='w-20' />
-                      )}
-                  </div>
-                  <div>
-                    {task.title}
-                  </div>
-                </div>
-              </td>
-              <td className="border border-gray-200 px-4 py-2">
+      <div className='grid grid-cols-2 md:grid-cols-2 gap-4 my-10'>
+        {!isLoadingTasks && tasks?.map((task) => (
+          <div key={task.id} className='p-4 rounded-md border-1 border space-y-4 shadow-lg'>
+            <div className='items-center space-x-2 flex-row flex'>
+              {task.feature_image_url && (
+                <img src={baseURL + task.feature_image_url} alt="Feature Image" className='w-20' />
+              )}
+              <div>
+                <h1 className='text-xl mb-2'>{task.title}</h1>
                 {task.completed_at ? (
-                  <span className="text-green-500">Completed</span>
+                  <span className="bg-green-500 p-1 text-white rounded-lg text-sm">Completed</span>
                 ) : (
-                  <span className="text-yellow-500">Pending</span>
+                  <span className="bg-yellow-500 p-1 text-white rounded-lg text-sm">Pending</span>
                 )}
-              </td>
-              <td className="border border-gray-200 px-4 py-2">
-                <div className="flex space-x-2">
-                  {!task.completed_at && (
-                    <button
-                      className="bg-blue-500 text-white p-2 rounded text-nowrap"
-                      onClick={() => completeTask(task)}
-                    >
-                      Mark as Completed
-                    </button>
-                  )}
-                  <Link
-                    to={`/tasks/${task.id}/edit`}
-                    className="bg-blue-500 text-white p-2 rounded"
-                  >
-                    Edit
-                  </Link>
-                  <button
-                    className="bg-red-500 text-white p-2 rounded"
-                    onClick={() => destroyTask(task)}
-                  >
-                    Destroy
-                  </button>
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+              </div>
+            </div>
+            
+            <div className="flex space-x-2">
+              {!task.completed_at && (
+                <Link
+                className="bg-blue-500 text-white p-1 rounded text-nowrap"
+                onClick={() => completeTask(task)}
+                >
+                  Mark as Completed
+                </Link>
+              )}
+              <Link
+                to={`/tasks/${task.id}/edit`}
+                className="bg-blue-500 text-white p-1 rounded"
+              >
+                Edit
+              </Link>
+              <Link
+                className="bg-red-500 text-white p-1 rounded"
+                onClick={() => destroyTask(task)}
+              >
+                Destroy
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
